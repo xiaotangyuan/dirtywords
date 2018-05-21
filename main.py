56c1d542dbb22fef3d5695e731ed060fd7e4efa5
luoyingtian <yt.luo@idiaoyan9.19.com>
2018-05-21 16:22:08 +0800

    url /checkcupleweightword

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request
 from wordutil import find_word_from_tree_dict, init_dirtyword_tree_dict
+import wordcupleweight
 
 
 app = Flask(__name__)
@@ -13,6 +14,17 @@ def checkword():
 	content = request.args.get('content')
 	words_list = find_word_from_tree_dict(content, return_all_dirty_words=True)
 	return json.dumps(words_list)
+
+@app.route('/checkcupleweightword', methods=['GET', 'POST'])
+def checkcupleweightword():
+	# content = request.form.get('content')
+	content = request.args.get('content')
+	max_weight, words_list = wordcupleweight.find_word_and_weight_from_tree_dict(content)
+	res = {
+		'max_weight': max_weight,
+		'words_list': words_list
+	}
+	return json.dumps(res)
 
 
 @app.route('/hello')
