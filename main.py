a3856e8ccc6f1f25bbde778441ed0397904342db
luoyingtian <yt.luo@idiaoyan9.19.com>
2018-06-13 17:37:12 +0800

    添加登录认证

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 import logging
+import flask
 from flask import Flask, request, redirect, url_for
 from flask import render_template
+import flask_login
 import wordutil
 import wordcupleweight
 
@@ -16,10 +18,73 @@ log = logging
 
 
 app = Flask(__name__)
+app.secret_key = 'dirtywordscheckerkey'
 wordutil.init_dirtyword_tree_dict_from_sqlite()
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+users = {'yt.luo@idiaoyan.com': {'password': 'IDY123456'}}
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('managewords'))
+
+    return 'Bad login'
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
 
 
 @app.route('/checkwords', methods=['GET', 'POST'])
+@flask_login.login_required
 def checkword():
     if request.method == 'POST':
         content = request.form.get('content')
@@ -40,6 +105,7 @@ def checkword():
 
 
 @app.route('/checkcupleweightword', methods=['GET', 'POST'])
+@flask_login.login_required
 def checkcupleweightword():
     content = request.form.get('content')
     max_weight, words_list = wordcupleweight.find_word_and_weight_from_tree_dict(content)
@@ -51,6 +117,7 @@ def checkcupleweightword():
 
 
 @app.route('/managewords', methods=['GET'])
+@flask_login.login_required
 def managewords():
     wordlib = request.args.get('wordlib')
     if not wordlib:
@@ -64,6 +131,7 @@ def managewords():
 
 
 @app.route('/submitwords', methods=['POST'])
+@flask_login.login_required
 def submitwords():
     wordlib = request.form.get('wordlib')
     words = request.form.get('words')
@@ -80,6 +148,7 @@ def submitwords():
 
 
 @app.route('/tree', methods=['get'])
+@flask_login.login_required
 def tree():
     tree_json = json.dumps(wordutil.tree_dict, ensure_ascii=False)
     return tree_json
